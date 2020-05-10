4649b3f6dc95e92193082b28a18b6cee50232d4f
lihongjie <lihongjie0209@gmail.com>
2020-05-10 17:27:49 +0800

    mbg 自动生成整合

--- springboot_mybatis/src/main/java/cn/lihongjie/springdemo/mybatis/controller/ProjectUserController.java
+++ springboot_mybatis/src/main/java/cn/lihongjie/springdemo/mybatis/controller/ProjectUserController.java
@@ -1,21 +1,33 @@
 package cn.lihongjie.springdemo.mybatis.controller;
 
+import cn.lihongjie.springdemo.mybatis.entity.generated.ProjectUserExample;
 import cn.lihongjie.springdemo.mybatis.mapper.UserMapper;
+import cn.lihongjie.springdemo.mybatis.mapper.generated.ProjectUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class ProjectUserController {
 
 
-    @Autowired
+    @Resource
     UserMapper userMapper;
 
+    @Resource
+    ProjectUserMapper projectUserMapper;
     @GetMapping(path = "/getall1")
     public Object getall() {
 
         return userMapper.getall();
+
+    }
+    @GetMapping(path = "/getall2")
+    public Object getall2() {
+
+        return projectUserMapper.selectByExample(new ProjectUserExample());
 
     }
 }
